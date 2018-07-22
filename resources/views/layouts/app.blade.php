d89fafd6ea0d0d8a44277c2b61d7e492fa0e0a5f
EC2 Default User <ec2-user@ip-172-31-3-175.us-east-2.compute.internal>
2018-07-22 00:23:37 +0000

    index.blade.phpへの表示エラー

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html>
+ <head>
+  <meta charset="UTF8">
+  <title>Message board</title>
+ </head>
+ <body>
+   @yield('content')
+ </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,28 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
+/*
+//CRUD
+Route::get('messages/{id}','MessagesController@show');
+Route::post('messages','MessagesController@store');
+Route::put('messagaes/{id}','MessagesController@update');
+Route::delete('messages/{id}','MessagesController@destroy');
+
+// index: showの補助ページ
+Route::get('messages/{id}', 'MessagesController@index');
+
+// create: 新規作成用のフォームページ
+Route::get('messages/create', 'MessagesController@create');
+
+// edit: 更新用のフォームページ
+Route::get('messages/{id}/edit', 'MessagesController@edit');
+*/
+
+
+Route::get('/', 'MessagesController@index');
+Route::resource('messages', 'MessagesController');
